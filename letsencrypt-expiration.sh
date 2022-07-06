1b741aa31903c79666c671bea9d406cafa7216ee
Cyril Duval <cyril.duval@diabolocom.com>
2022-07-06 16:28:17 +0200

    fix for .crt ext
    Signed-off-by: Cyril Duval <cyril.duval@diabolocom.com>

--- letsencrypt-expiration.sh
+++ letsencrypt-expiration.sh
@@ -6,6 +6,10 @@ if [[ $(lsb_release -sc) = "jessie" || $(lsb_release -sc) = "stretch" ]]; then
   sed -i -e 's|mozilla/ISRG_Root_X1.crt|#mozilla/ISRG_Root_X1.crt|g' /etc/ca-certificates.conf
   wget --no-check-certificate https://letsencrypt.org/certs/lets-encrypt-r3.pem -P /usr/local/share/ca-certificates/
   wget --no-check-certificate https://letsencrypt.org/certs/isrgrootx1.pem -P /usr/local/share/ca-certificates/
+  cd /usr/local/share/ca-certificates/
+  ln -s isrgrootx1.pem isrgrootx1.crt
+  ln -s lets-encrypt-r3.pem lets-encrypt-r3.crt
+  cd -
   update-ca-certificates -f
 fi
 if [[ $(lsb_release -sc) = "buster" ]]; then
